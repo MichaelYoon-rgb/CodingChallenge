dad3a05c7dd47eb5304ba6c8d584c9ce478c484d
Feiming <69386134+Eyrenbend@users.noreply.github.com>
2022-12-22 16:17:19 +0000

    Update VA1.py
    Cleaned code

--- VA1.py
+++ VA1.py
@@ -1,77 +1,91 @@
-import os
 import speech_recognition as sr
-import playsound
 import pyttsx3
 import random
 import requests
-import datetime
-import requests, json
-api_key = "e6060d7f0ba818f039bc37878ad0c029"
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
-complete_url = base_url + "appid=" + api_key + "&q=" + "London"
-response = requests.get(complete_url)
-x = response.json()
-if x["cod"] != "404":
-    y = x["main"]
-    current_temperature = y["temp"]
-    current_pressure = y["pressure"]
-    current_humidity = y["humidity"]
-    z = x["weather"]
-    weather_description = z[0]["description"]
-    print(" Temperature (Celcius) = " +
-          str(round(current_temperature-272.15,2)) +
-          "\n atmospheric pressure (in hPa unit) = " +
-          str(current_pressure) +
-          "\n humidity (in percentage) = " +
-          str(current_humidity) +
-          "\n description = " +
-          str(weather_description))
-
 
 jokes = ["Why should you never fight a dinosaur? Because you'll get jurasskicked. Hahahahahaha", ""]
 
-r=sr.Recognizer()
-print(sr.Microphone.list_microphone_names())
-engine = pyttsx3.init()
-voices = engine.getProperty("voices")
-rate = engine.getProperty("rate")
-engine.setProperty("rate", 100)
-engine.setProperty("voice", voices[1].id)
+def init_engine():
+    engine = pyttsx3.init()
+    voices = engine.getProperty("voices")
+    engine.setProperty("rate", 150)
+    engine.setProperty("voice", voices[1].id)
 
-def sound(text):
+    return engine
+
+def get_current_weather(city):
+    api_key = "e6060d7f0ba818f039bc37878ad0c029"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    url = base_url + "appid=" + api_key + "&q=" + city
+    
+    response = requests.get(url)
+    results = response.json()
+
+    if results["cod"] != "404":
+        results_main = results["main"]
+        
+        results_main_temperature = results_main["temp"]
+        results_main_pressure = results_main["pressure"]
+        results_main_humidity = results_main["humidity"]
+
+        results_weather = results["weather"]
+        results_weather_description = results_weather[0]["description"]
+
+        return f"""The temperature is {round(results_main_temperature-272.15,2)} degrees celcius
+        The atmospheric pressure is {results_main_pressure} hectopascals
+        The humidity is {results_main_humidity} percent
+        Description: {results_weather_description}"""
+            
+def get_random_joke():
+    return random.choice(jokes)
+
+def get_va_name():
+    return "My name is Pasham"
+
+def log_microphones():
+    print(sr.Microphone.list_microphone_names())
+
+
+def say(text):
     engine.say(text)
     engine.runAndWait()
 
+def output(text):
+    say(text)
+    print(text)
 
-def get_audio():
-    
+def listen():
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source,duration=1)
-        print("say anything : ")
-        audio= r.listen(source)
-        text= ""
-        try:
-            text = r.recognize_google(audio)
-            print(text)
-        except:
-            print("sorry, could not recognise")
-    return text
-    
-while True:
-    text2 = get_audio()
-    if "hello" in text2:
-        sound("Hello")
-    if "what is your name" in text2:
-        sound("My name is Pasham")
-    if "tell me a joke" in text2:
-        sound(random.choice(jokes))
-    if "tell me weather" in text2:
-        sound(" Temperature (Celcius) = " +
-          str(round(current_temperature-272.15,2)) +
-          "\n atmospheric pressure (in hPa unit) = " +
-          str(current_pressure) +
-          "\n humidity (in percentage) = " +
-          str(current_humidity) +
-          "\n description = " +
-          str(weather_description))
+        recogniser.adjust_for_ambient_noise(source,duration=1)
 
+        audio = recogniser.listen(source)
+        text = ""
+
+        try:
+            text = recogniser.recognize_google(audio)
+            print(text)
+
+        except:
+            output("Sorry, I didn't quite hear that. Could you say that again?")
+
+    return text
+   
+recogniser =sr.Recognizer()
+log_microphones()
+engine = init_engine()
+
+output("Hello, how can I help you?")   
+
+while True:
+    speech_input = listen()
+    if "hello" in speech_input:
+        output("Hello")
+    
+    elif "what is your name" in speech_input:
+        output(get_va_name())
+    
+    elif "tell me a joke" in speech_input:
+        output(get_random_joke())
+    
+    elif "tell me weather" in speech_input:
+        output(get_current_weather("London"))
