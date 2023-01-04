3846e135ba11261d326c947743537eb9cde1943d
于飞鸣 <69386134+Eyrenbend@users.noreply.github.com>
2023-01-04 14:36:41 +0000

    Update main.js
    Optimised handling the responses

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var responses = [
   {
-    Keyword: ["Joke"],
+    Keyword: ["joke"],
     Response: [
       "Which is faster, hot or cold? Hot, because you can catch cold.",
       "What did one plate say to the other plate? Dinner's on me.",
@@ -13,11 +13,11 @@ var responses = [
     ]
   },
   {
-    Keyword: ["Weather", "Temperature"],
+    Keyword: ["Weather", "temperature"],
     Response: []
   },
   {
-    Keyword: ["Recipe"],
+    Keyword: ["recipe"],
     Response: [
       "Shakshuka<br><br>" + "Ingredients:<br>3 tbsp olive oil<br>2 large onions thinly sliced<br>2 red peppers cut into long slices<br>2 green peppers cut into long slices<br>4 garlic cloves finely chopped<br>½ tsp cumin seeds<br>½ tsp caraway seeds<br>½ tsp cayenne pepper<br>1 tbsp tomato or red pepper purée<br>2 400 grams tins tomatoes<br>1 small bunch fresh coriander roughly chopped<br>1 small bunch fresh parsley roughly chopped<br>8 free-range eggs<br>85 grams crumbled feta<br>8 tbsp thick natural yoghurt or labneh<br>salt and freshly ground black pepper.<br><br>" + "Steps:<br>1. Heat the olive oil in a large, lidded frying pan. Add the onions and peppers and season with salt and pepper. Cook on a medium heat until just softened. Add the garlic and cook for a further 2 minutes. Sprinkle in the cumin and caraway seeds and the cayenne pepper. Stir in the tomato or red pepper purée and cook for a couple more minutes until the paste starts to separate. Add the tomatoes with a splash of water.<br>2. Simmer for 10 minutes, uncovered, until reduced a little. Taste after 5 minutes and add a little sugar if you think the tomatoes need it. Keep an eye on the texture – you don’t want it runny, but it mustn’t get too dry either. Add another splash of water if necessary. When the sauce is reduced, stir in the herbs.<br>3. Make 8 small wells in the sauce. Break an egg into a cup and drop carefully into a well, repeat with the remaining eggs. Cook for a few more minutes until the whites are just set and the yolks are still runny. Sprinkle over the crumbled feta. Serve with yogurt or labneh on the side.",
       "Banana Bread<br><br>" + "Ingredients:<br>285 grams plain flour<br>1 tsp bicarbonate of soda<br>½ tsp salt<br>110 grams butter, plus extra for greasing<br>225 grams caster sugar<br>2 free-range eggs<br>4 ripe bananas, mashed<br>85ml buttermilk (or normal milk mixed with 1½ tsp lemon juice or vinegar)<br>1 tsp vanilla extract<br><br>" + "Steps:<br>1. Preheat the oven to 180 Celcius/350 Fahrenheit/Gas 4.<br>2. Sift the flour, bicarbonate of soda and salt into a large mixing bowl.<br>3. In a separate bowl, cream the butter and sugar together until light and fluffy.<br>4. Add the eggs, mashed bananas, buttermilk and vanilla extract to the butter and sugar mixture and mix well. Fold in the flour mixture.<br>5. Grease a 20cm x 12.5cm loaf tin and pour the banana bread mixture into the tin.<br>6. Transfer to the oven and bake for about an hour, or until well-risen and golden-brown.<br>7. Remove from the oven and cool in the tin for a few minutes, then turn out onto a wire rack to cool completely before serving.", 
@@ -26,7 +26,7 @@ var responses = [
     ]
   },
   {
-    Keyword: ["Birthday"],
+    Keyword: ["birthday"],
     Response: [
       "Happy Birthday to you<br>Happy Birthday to you<br>Happy Birthday dear (name)<br>Happy Birthday to you."
     ]
@@ -168,32 +168,35 @@ function activateSpeechRecognition() {
 function handleResponses(input) {
   let newInput = input;
 
-  newInput = newInput.replace(".", " ");
-  newInput = newInput.split(" ");
+  newInput = newInput.replace(".", " ").toLowerCase();
 
-  for (let wordIndex = 0; wordIndex < newInput.length; wordIndex++) {
-    let currentWord = newInput[wordIndex];
-    for (
-      let responseIndex = 0;
-      responseIndex < responses.length;
-      responseIndex++
-    ) {
-      let keywords = responses[responseIndex]["Keyword"];
-      for (
-        let keywordIndex = 0;
-        keywordIndex < keywords.length;
-        keywordIndex++
-      ) {
-        if (keywords[keywordIndex].toLowerCase() == currentWord.toLowerCase()) {
-          let validResponses = responses[responseIndex]["Response"];
-
-          return validResponses[
-            Math.floor(Math.random() * validResponses.length)
-          ];
-        }
-      }
+  for(let responseIndex = 0; responseIndex < responses.length; responseIndex++){
+    if(responses[responseIndex]["Keyword"].some(keyword => newInput.includes(keyword))){
+      let validResponses = responses[responseIndex]["Response"];
+  
+      return validResponses[
+        Math.floor(Math.random() * validResponses.length)
+      ];
     }
   }
+  
+  //for (let wordIndex = 0; wordIndex < newInput.length; wordIndex++) {
+  //  let currentWord = newInput[wordIndex];
+  //  
+  //  for (let responseIndex = 0; responseIndex < responses.length; responseIndex++) {
+  //    let keywords = responses[responseIndex]["Keyword"];
+  //    
+  //    for (let keywordIndex = 0; keywordIndex < keywords.length; keywordIndex++) {
+  //      if (keywords[keywordIndex].toLowerCase() == currentWord.toLowerCase()) {
+  //        let validResponses = responses[responseIndex]["Response"];
+  //
+  //        return validResponses[
+  //          Math.floor(Math.random() * validResponses.length)
+  //        ];
+  //      }
+  //    }
+  //  }
+  //}
   return "";
 }
 
