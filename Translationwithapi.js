f1128c311b5bb10110096736945f88e5556be9a1
于飞鸣 <69386134+Eyrenbend@users.noreply.github.com>
2023-01-06 22:14:41 +0000

    Delete Translationwithapi.js

--- Translationwithapi.js
+++
@@ -1,6 +0,0 @@
-let text = document.getElementById("greeting")
-
-const apiURL = `https://api.mymemory.translated.net/get?q=${text}&langpair=en-GB|de-DE`;
-const response = await fetch(apiURL)
-const data = await response.json();
-document.write(data["responseData"]["translatedText"])
