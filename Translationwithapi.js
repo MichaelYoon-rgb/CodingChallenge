4908f2085d90e1e3eeaffd50ab66f5b480c46750
于飞鸣 <69386134+Eyrenbend@users.noreply.github.com>
2023-01-06 15:16:29 +0000

    Update Translationwithapi.js
    Implemented obtaining translation from api

--- Translationwithapi.js
+++ Translationwithapi.js
@@ -1,5 +1,6 @@
 let text = document.getElementById("greeting")
 
-let apiURL = `https://api.mymemory.translated.net/get?q=${text}&langpair=en-GB|de-DE`;
-let response = fetch(apiURL)
-document.write(response["translation"])
+const apiURL = `https://api.mymemory.translated.net/get?q=${text}&langpair=en-GB|de-DE`;
+const response = await fetch(apiURL)
+const data = await response.json();
+document.write(data["responseData"]["translatedText"])
